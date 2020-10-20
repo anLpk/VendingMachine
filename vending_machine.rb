00924d1ffb12c2a32d77bad18b55fce6114f9018
Anil Pak <71243066+anLpk@users.noreply.github.com>
2020-10-20 22:08:29 +1100

    Update vending_machine.rb

--- vending_machine.rb
+++ vending_machine.rb
@@ -64,23 +64,3 @@ puts display(vending_machine)
 puts "Pushing 'Buy Snack' button"
 vending_machine.buy_snack
 puts display(vending_machine)
-
-#Refilling Scenario
-
-require_relative "vending_machine"
-
-def display(vending_machine)
-  return "---> Amount: #{vending_machine.user_balance_cents / 100.0}€" \
-  " - 1 Snack = #{vending_machine.snack_price_cents / 100.0}€" \
-  " - Stock: #{vending_machine.snack_count}"
-end
-
-# We instantiate an **empty** vending machine selling 2.5 euros snacks.
-vending_machine = VendingMachine.new(250, 0)
-puts "Vending machine ready!"
-puts display(vending_machine)
-
-# A technician comes and refill the vending machine with 20 snacks.
-puts "Technician filling machine with 20 snacks"
-vending_machine.snack_count = vending_machine.snack_count + 20
-puts display(vending_machine)
